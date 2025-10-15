062d024cfc881b89b56f1810d5d6c29442a65c60
Gabriel <usr4035@salesianos-lacuesta.net>
2025-10-15 10:59:29 +0100

    Puse el nuevo codigo en CardSeconds

--- components/CardSeconds.jsx
+++ components/CardSeconds.jsx
@@ -1,3 +1,27 @@
 import React from 'react';
-import { View, Text, StyleSheet, Image } from 'react-native';
+import { View, Image, StyleSheet } from 'react-native';
 import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
+
+const CardSeconds = ({ title, subtitle, author, imageUno, imageDos, imageSource, body, textUno, textDos }) => {
+  return (
+    <View style={styles.card}>
+      {imageUno && (
+        <View style={styles.imageContainer}>
+          <Image source={imageUno} style={styles.image} />
+        </View>
+      )}
+      {imageDos && (
+        <View style={styles.imageContainer}>
+          <Image source={imageDos} style={styles.image} />
+        </View>
+      )}
+      {imageSource && (
+        <View style={styles.imageContainer}>
+          <Image source={imageSource} style={styles.image} />
+        </View>
+      )}
+    </View>
+  );
+};
+
+export default CardSeconds;
